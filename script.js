20a614bdb9f86c27aaba594b5d7d60e2bcf6aef5
Umidjon Amirkhulov[FE17] <122218906+umidjon20@users.noreply.github.com>
2023-01-26 09:33:50 -0700

    last changes

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ upperFirst("umid")
 function getCardNum(cardNum){
   
 
+ if(cardNum.length == 0 ){
+        alert('-You entered anything-')
+    }else if(cardNum.length > 16 || cardNum.length < 16){
+        alert(-'You entered incorrect number-')
+    }else if(isNaN(+cardNum)){
+        alert('You entered string\n -Please enter the number-')
+    }else if(cardNum.slice(0, 4) == '8600'){
+        alert(cardNum.slice(0, 4) + ' ' + '****' + '****' + ' ' + cardNum.slice(12, 16) + '\nYour card is -Uzcard-')
+    }else if(cardNum.slice(0, 4) == '9860'){
+        alert(cardNum.slice(0, 4) + ' ' + '****' + '****' + ' ' + cardNum.slice(12, 16) + '\nYour card is -Humo-')
+    }else{
+        alert('-This card wasn`t found-')
+    }
 
 }
 
